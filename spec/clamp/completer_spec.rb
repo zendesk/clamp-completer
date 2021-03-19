a7c2fc4742c992f6bc4ae742d9be9d6b43dec3ea
Vignesh <vignesh@rvignesh.io>
2021-03-19 18:27:00 +0800

    Fix autocompletion generation when sub-command has hyphens (#1)
    * Extract subcommand_name local var
    * Fix erb issue and write a test

--- spec/clamp/completer_spec.rb
+++ spec/clamp/completer_spec.rb
@@ -1,5 +1,123 @@
+require 'clamp'
+
 RSpec.describe Clamp::Completer do
+  include OutputCapture
+  extend CommandFactory
+
   it "has a version number" do
     expect(Clamp::Completer::VERSION).not_to be nil
+  end
+
+  given_command("cmd") do
+    subcommand "complete", "shell autocompletions", Clamp::Completer.new(self)
+    subcommand "render-profile", "Command with hyphen" do
+      def execute
+        puts "Command with hyphen"
+      end
+    end
+  end
+
+  it "generates zsh autocompletion" do
+    command.run(%w[complete zsh])
+    expect(stdout).to eq(%{#compdef rspec
+
+
+_rspec_complete_subcommands() {
+  return 1
+}
+
+
+_rspec_complete() {
+  local curcontext="$curcontext" state state_descr line expl
+  local ret=1
+
+  _arguments -C : \\
+    '1:subcommands:_rspec_complete_subcommands' \\
+            {-h,--help}'[print help]' \\
+    '*::options:->options' && return 0
+
+  case "$state" in
+    command) _rspec_complete_subcommands && return 0 ;;
+    options)
+      local command="${line[1]}"
+      curcontext="${curcontext%:*:*}:rspec_complete-${command}"
+
+      line=(${line:1})
+      local completion_func="_rspec_complete_${command//-/_}"
+      _call_function ret "${completion_func}" && return ret
+
+      _message "a completion function is not defined for ${command}"
+      return 1
+    ;;
+  esac
+}
+
+
+_rspec_render_profile_subcommands() {
+  return 1
+}
+
+
+_rspec_render_profile() {
+  local curcontext="$curcontext" state state_descr line expl
+  local ret=1
+
+  _arguments -C : \\
+    '1:subcommands:_rspec_render_profile_subcommands' \\
+            {-h,--help}'[print help]' \\
+    '*::options:->options' && return 0
+
+  case "$state" in
+    command) _rspec_render_profile_subcommands && return 0 ;;
+    options)
+      local command="${line[1]}"
+      curcontext="${curcontext%:*:*}:rspec_render_profile-${command}"
+
+      line=(${line:1})
+      local completion_func="_rspec_render_profile_${command//-/_}"
+      _call_function ret "${completion_func}" && return ret
+
+      _message "a completion function is not defined for ${command}"
+      return 1
+    ;;
+  esac
+}
+
+
+_rspec_subcommands() {
+  local subcommands=( 'complete:shell autocompletions' 'render-profile:Command with hyphen' )
+  _describe 'subcommands' subcommands && return 0
+  return 1
+}
+
+
+_rspec() {
+  local curcontext="$curcontext" state state_descr line expl
+  local ret=1
+
+  _arguments -C : \\
+    '1:subcommands:_rspec_subcommands' \\
+            {-h,--help}'[print help]' \\
+    '*::options:->options' && return 0
+
+  case "$state" in
+    command) _rspec_subcommands && return 0 ;;
+    options)
+      local command="${line[1]}"
+      curcontext="${curcontext%:*:*}:rspec-${command}"
+
+      line=(${line:1})
+      local completion_func="_rspec_${command//-/_}"
+      _call_function ret "${completion_func}" && return ret
+
+      _message "a completion function is not defined for ${command}"
+      return 1
+    ;;
+  esac
+}
+
+
+compdef _rspec rspec
+})
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,38 @@
 require "bundler/setup"
 require "clamp/completer"
 
+module CommandFactory
+  def given_command(name, &block)
+    let(:command_class) do
+      Class.new(Clamp::Command, &block)
+    end
+    let(:command) do
+      command_class.new(name)
+    end
+  end
+end
+module OutputCapture
+  def self.included(target)
+    target.before do
+      $stdout = @out = StringIO.new
+      $stderr = @err = StringIO.new
+    end
+
+    target.after do
+      $stdout = STDOUT
+      $stderr = STDERR
+    end
+  end
+
+  def stdout
+    @out.string
+  end
+
+  def stderr
+    @err.string
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
